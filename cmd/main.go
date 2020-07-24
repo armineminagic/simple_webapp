efa9c7bb6b6b2d14aac9d5cf5c5a2ee9eecfe176
Armin Eminagic <armine@bicomsystems.com>
2020-07-24 14:52:04 +0200

    - Ordered files by .

---
+++ cmd/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	_ "fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+// CmdArgs holds values about command line flags
+type CmdArgs struct {
+	DbName   string
+	User     string
+	Password string
+}
+
+// cmdArgs for db connection info
+var cmdArgs CmdArgs
+
+// Student holds information about stud
+type Student struct {
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Indexnum uint   `json:"indexnum"`
+	IDnumber uint64 `json:"id"`
+}
+
+// ifError simple funciton for checks if error exists
+func ifError(e error) {
+	if e != nil {
+		log.Panic(e.Error())
+	}
+}
+
+// DbConn creates DB and table if not exists
+func DbConn(ca CmdArgs) (db *sql.DB) {
+
+	// opening connection on mysql server and connecting on database name read from command line flags
+	db, err := sql.Open("mysql", ca.User+":"+ca.Password+"@tcp(127.0.0.1:3306)/?charset=utf8&autocommit=true")
+	ifError(err)
+
+	_, err = db.Exec(`create database if not exists ` + ca.DbName + `;`)
+	ifError(err)
+
+	_, err = db.Exec(`use ` + ca.DbName + `;`)
+	ifError(err)
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS `students` (`index` INTEGER NOT NULL, `idstud` INTEGER NOT NULL, `name` VARCHAR(30) not null, `surname` VARCHAR(30) not null);")
+	ifError(err)
+
+	return db
+}
+
+// Students handler for homepage
+func getStudents(w http.ResponseWriter, r *http.Request) {
+	db := DbConn(cmdArgs)
+	defer db.Close()
+	rows, err := db.Query("Select * from students")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stud := Student{}
+	students := []Student{}
+	for rows.Next() {
+		err = rows.Scan(&stud.Indexnum, &stud.IDnumber, &stud.Name, &stud.Surname)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		students = append(students, stud)
+	}
+	studentsJSON, err := json.Marshal(students)
+	ifError(err)
+	w.Write(studentsJSON)
+}
+
+// searcStudent handler for finding students with explicit Index number
+func searchStudent(w http.ResponseWriter, r *http.Request) {
+
+	searchValue := mux.Vars(r)
+
+	db := DbConn(cmdArgs)
+	defer db.Close()
+
+	row, err := db.Query("Select * from students where `index`= ?", searchValue["indexnum"])
+	ifError(err)
+
+	// collecting info about student and storing it in a students array
+	student := Student{}
+	students := []Student{}
+	for row.Next() {
+		err = row.Scan(&student.Indexnum, &student.IDnumber, &student.Name, &student.Surname)
+		ifError(err)
+		students = append(students, student)
+	}
+	studentsJSON, err := json.Marshal(students)
+	ifError(err)
+	w.Write(studentsJSON)
+
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		log.Println(r.Method)
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+
+	flag.StringVar(&cmdArgs.User, "u", "root", "database connection user")
+	flag.StringVar(&cmdArgs.Password, "p", "root", "database connection password")
+	flag.StringVar(&cmdArgs.DbName, "db", "faculty", "database name")
+	flag.Parse()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", getStudents)
+	r.HandleFunc("/search/{indexnum}", searchStudent)
+	r.Use(middleware)
+
+	log.Println("Server started...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
